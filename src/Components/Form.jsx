cc19bd9fb6076691c518914ad0fc48242c266876
henry-cv <thechosenone.cc@gmail.com>
2024-11-30 13:44:18 -0500

    FE3 TPI, creación de formulario

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,13 +1,34 @@
-import React from "react";
-
-
+import { useState } from "react";
 const Form = () => {
   //Aqui deberan implementar el form completo con sus validaciones
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
 
+
+  const readFullname = (e) => {
+    let inputName = e.target.value.trim();
+    return /[a-záéíóúñ]{6,}/ig.test(inputName)
+      ? setFullname(inputName)
+      : ""
+  }
+  const readEmail = (e) => {
+    let inputEmail = e.target.value.trim();
+    return /^[\w]{4,}@[a-z]{3,}\.[a-z]{2,4}$/.test(inputEmail)
+      ? setEmail(inputEmail)
+      : ""
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (email && fullname) console.info(`Gracias ${fullname}, te contactaremos cuando antes vía mail`)
+    else console.warn("Por favor verifique su información nuevamente")
+  }
   return (
     <div>
       <h2>Formulario</h2>
-      <form>
+      <form className="formulario" onSubmit={handleSubmit}>
+        <input onChange={(e) => readFullname(e)} type="text" placeholder="Nombre completo" required autoComplete="on" />
+        <input onChange={(e) => readEmail(e)} type="text" placeholder="Email" />
+        <input type="submit" value="Enviar" />
       </form>
     </div>
   );
--- src/Routes/Detail.jsx
+++ src/Routes/Detail.jsx
@@ -10,27 +10,32 @@ const Detail = () => {
   const url = `https://jsonplaceholder.typicode.com/users/${id}`;
   console.log(`id: ${id}`);
 
-  const getDentist = async () => {
-    const res = await fetch(url);
-    console.log(res);
-    const data = await res.json();
-    console.log(data);
-    setDentist(data);
+  const getDentist = () => {
+    axios(url)
+      .then((res) => {
+        console.log(res);
+        setDentist(res.data);
+      }).
+      catch(err => console.error(err));
   }
   console.log("dentista:")
   console.log(dentist);
 
   useEffect(() => {
     getDentist();
-  }, [dentist]);
+  }, []);
   return (
     <>
       <h1>Detail Dentist {id} </h1>
       {/* aqui deberan renderizar la informacion en detalle de un user en especifico */}
-      <h2>{dentist.name}</h2>
-      <h2>{dentist.email}</h2>
-      <h2>{dentist.phone}</h2>
-      <h2>{dentist.website}</h2>
+      <article className="card">
+        <h3>{dentist.name}</h3>
+        <img src="../../public/images/doctor.jpg" alt="foto" width="200px" />
+        <h3>{dentist.email}</h3>
+        <h3>{dentist.phone}</h3>
+        <h3>{dentist.website}</h3>
+      </article>
+
       {/* Deberan mostrar el name - email - phone - website por cada user en especifico */}
     </>
   )
