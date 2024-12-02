302ee2e1bf2a528ea5db74c4a2c48a3601c1e5c0
henry-cv <thechosenone.cc@gmail.com>
2024-12-02 11:38:53 -0500

    TPI, corregida href en imgs

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -11,13 +11,13 @@ const Card = ({ name, username, id }) => {
       {/* En cada card deberan mostrar en name - username y el id */}
       <Link to={`/dentista/${id}`} >
         <h3>{name}</h3>
-        <img src="../../public/images/doctor.jpg" alt="dentista" width="100px" />
+        <img src="/images/doctor.jpg" alt="dentista" width="100px" />
         <h4>{username}</h4>
 
         {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
       </Link>
       {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
-      <button onClick={addFav} className="favButton">Add fav</button>
+      <button onClick={addFav} data-id={id}  className="favButton">Add fav</button>
     </div >
   );
 };
