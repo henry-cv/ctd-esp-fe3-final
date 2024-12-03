41899767a06991799b776373711129c8647cb765
henry-cv <thechosenone.cc@gmail.com>
2024-12-03 12:39:14 -0500

    FE3 TPI, formulario listo con validaciones

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -4,8 +4,9 @@ const Form = () => {
   const [visitor, setVisitor] = useState({
     fullname: "", email: ""
   })
-  const ErrorMessage = "Por favor verifique su información nuevamente";
   const [error, setError] = useState(false);
+  const ErrorMessage = "Por favor verifique su información nuevamente";
+  const SuccessMessage = `Gracias ${visitor.fullname}, te contactaremos cuando antes vía mail`;
 
   const readFullname = (e) => {
     let inputName = e.target.value.trim();
@@ -23,24 +24,35 @@ const Form = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (visitor.fullname && visitor.email) {
+    const expValidName = /[a-záéíóúñ]{6,}/ig
+    const expValidEmail = /^[\w]{4,}@[a-z]{3,}\.[a-z]{2,4}$/
+    if (expValidName.test(visitor.fullname.trim()) && expValidEmail.test(visitor.email.trim())) {
+      console.info(`Gracias ${visitor.fullname}, te contactaremos cuando antes vía mail`);
+      setError(false)
+    }
+    else {
+      console.log(ErrorMessage);
+      setError(true);
+    }
+    /* if (visitor.fullname && visitor.email) {
       console.info(`Gracias ${visitor.fullname}, te contactaremos cuando antes vía mail`);
       setError(false);
     }
     else {
       setError(true);
-    }
+    } */
   }
 
   return (
     <div>
       <h2>Formulario</h2>
       <form className="formulario" onSubmit={handleSubmit}>
-        <input type="text" onBlur={(e) => readFullname(e)} placeholder="Nombre completo" required autoComplete="On" title="Fullname must have at least 6 characters" />
-        <input type="email" onBlur={(e) => readEmail(e)} placeholder="Email" title="Email must have 4 characters before @"
+        <input type="text" onChange={(e) => setVisitor({ ...visitor, fullname: e.target.value })} placeholder="Nombre completo" required autoComplete="On" title="Fullname must have at least 6 characters" />
+        <input type="email" onChange={(e) => setVisitor({ ...visitor, email: e.target.value })} placeholder="Email" title="Email must have 4 characters before @"
           required autoComplete="On" />
         <input type="submit" value="Send" />
         {error && <p>{ErrorMessage}</p>}
+        {!error && <p>{SuccessMessage}</p>}
       </form>
     </div>
   );
