4c373a010359be469b31c7b83563043448c147a5
henry-cv <thechosenone.cc@gmail.com>
2024-12-06 22:04:32 -0500

    TPI, solucionado mensaje en formulario

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,38 +1,50 @@
 import { useState } from "react";
 import { form } from "../styles/App.module.css";
+import { useContextGlobal } from "../Components/utils/global.context";
+
 const Form = () => {
   //Aqui deberan implementar el form completo con sus validaciones
   const [visitor, setVisitor] = useState({
     fullname: "", email: ""
-  })
+  });
+  const [show, setShow] = useState(false);
+  const { state } = useContextGlobal();
+  const { theme } = state;
+
   const [error, setError] = useState(false);
   const ErrorMessage = "Por favor verifique su información nuevamente";
   const SuccessMessage = `Gracias ${visitor.fullname}, te contactaremos cuando antes vía mail`;
 
+  const validateFields = () => {
+    const expValidName = /^[a-záéíóúñ]{6,}$/ig
+    const expValidEmail = /^[\w]{4,}@[a-z]{3,}\.[a-z]{2,4}$/
+    return expValidName.test(visitor.fullname.trim()) && expValidEmail.test(visitor.email.trim());
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
-    const expValidName = /[a-záéíóúñ]{6,}/ig
-    const expValidEmail = /^[\w]{4,}@[a-z]{3,}\.[a-z]{2,4}$/
-    if (expValidName.test(visitor.fullname.trim()) && expValidEmail.test(visitor.email.trim())) {
-      console.info(`Gracias ${visitor.fullname}, te contactaremos cuando antes vía mail`);
-      setError(false)
-    }
-    else {
-      console.log(ErrorMessage);
+    if (validateFields()) {
+      console.info(SuccessMessage);
+      setError(false);
+
+    } else {
+      console.error(ErrorMessage);
       setError(true);
     }
+    setShow(true);
   }
 
   return (
-    <div>
-      <form className={form} onSubmit={handleSubmit}>
-        <input type="text" onChange={(e) => setVisitor({ ...visitor, fullname: e.target.value })} placeholder="Nombre completo" required autoComplete="On" title="Fullname must have at least 6 characters" />
-        <input type="email" onChange={(e) => setVisitor({ ...visitor, email: e.target.value })} placeholder="Email" title="Email must have 4 characters before @"
-          required autoComplete="On" />
-        <input type="submit" value="Send" />
-        {error ? <p>{ErrorMessage}</p> : <p>{SuccessMessage}</p>}
-      </form>
-    </div>
+    <form className={`${form} ${theme}`} onSubmit={handleSubmit}>
+      <input type="text" onChange={(e) => setVisitor({ ...visitor, fullname: e.target.value })} placeholder="Nombre completo" required autoComplete="on" title="Fullname must have at least 6 characters" />
+      <input type="email" onChange={(e) => setVisitor({ ...visitor, email: e.target.value })} placeholder="Email" title="Email must have 4 characters before @"
+        required autoComplete="on" />
+      <input type="submit" className={theme} value="Send" />
+      {show && (
+        <p style={{ color: error ? "red" : "green" }}>
+          {error ? ErrorMessage : SuccessMessage}
+        </p>
+      )}
+    </form>
   );
 };
 
--- src/Components/utils/reducer.js
+++ src/Components/utils/reducer.js
@@ -3,18 +3,18 @@ export const reducer = (state, action) => {
     case "CHANGE_THEME":
       return { ...state, theme: action.payload };
     case "ADD_FAV":
-    //return { ...state, favs:[state.favs, action.payload ]};
-      return { ...state, data:[...state.data, action.payload ]};
+      //return { ...state, favs:[state.favs, action.payload ]};
+      return { ...state, data: [...state.data, action.payload] };
     case "RESET_FAVS":
-      return {...state, data:[]}
-    case "REMOVE_FAV":
-      const index = action.dataId
-      const {data} = state;
-      console.log(index);
-      console.log("data antes de hacer el slice: ")
-      console.log(data)
-      return {...state, data:[...data.slice(0,index),
-        ...data.slice(index+1)]}
+      return { ...state, data: [] }
+    case "TOGGLE_FAV":
+      const exists = state.data.some((item) => item.id === action.payload.id);
+      return {
+        ...state,
+        data: exists
+          ? state.data.filter((item) => item.id !== action.payload.id) // Elimina si existe
+          : [...state.data, action.payload], // Agrega si no existe
+      };
     default:
       return state;
     //throw new Error("Acción no existente");
